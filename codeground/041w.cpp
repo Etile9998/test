9a4ad2e779114fdf6f70747b36bc6ab709efef4e
Etile9998 <gosehwan@gmail.com>
2017-06-16 03:07:47 +0900

    041w

--- codeground/041w.cpp
+++ codeground/041w.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
 const int mod = 1e9+7;
 #define rep(i,a,b) for(int i=a;i<b;++i)
 #define re(i,a) rep(i,0,a-1)
@@ -10,7 +11,15 @@ const int mod = 1e9+7;
 #define fi first
 #define se second
 
+int T;
+ld P;
+int a[5];
+
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>T;
+    rep(tc,1,T){
+        cin>>P;
+    }
     return 0;
 }
