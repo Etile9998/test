a93cb1dc27bf22afccb9194b923de5fe503cdc6f
Etile9998 <gosehwan@gmail.com>
2017-11-04 01:56:17 +0900

    Update 0.cpp

--- aoj/0.cpp
+++ aoj/0.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-const int MOD = 1e9+7;
+using ll = long long;
+const int MOD = 1e9 + 7;
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n) FOR(i,0,n)
 #define FORE(it,x) for(typeof(x.begin()) it=x.begin();it!=x.end();++it)
@@ -11,9 +11,11 @@ const int MOD = 1e9+7;
 #define CLEAR(x,with) memset(x,with,sizeof(x))
 #define sz size()
 
-int main(){
-    freopen("input.txt","r",stdin);
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    
-    return 0;
+int main() {
+#ifdef _CONSOLE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	return 0;
 }
